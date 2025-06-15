6bc4a5a8bc4856c0fb71784475befe42de26e2a8
Do Duc Minh <22010176@st.phenikaa-uni.edu.vn>
2025-06-15 15:24:17 +0700

    Init

---
+++ src/lib/server/db/schema.js
@@ -0,0 +1,33 @@
+import { bigint, foreignKey, timestamp } from 'drizzle-orm/gel-core';
+import { int, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core';
+
+// export const user = mysqlTable('user', {
+// 	id: serial('id').primaryKey(),
+// 	age: int('age')
+// });
+
+export const BangCap = mysqlTable('BangCap', {
+	id: serial('id').primaryKey(),
+	tenBC: varchar("tenBC", { length: 255 }).unique().notNull(),
+	tenVietTat: varchar("tenVietTat", { length: 255 }).unique().notNull(),
+	heSo: bigint('heSo', { mode: 'number' }).default(1)
+})
+
+export const Khoa = mysqlTable("Khoa", {
+	id: serial('id').primaryKey(),
+	tenKhoa: varchar("tenKhoa", { length: 255 }).unique().notNull(),
+	tenVietTat: varchar("tenVietTat", { length: 255 }).unique().notNull(),
+	moTaNhiemVu: varchar("moTaNhiemVu", { length: 255 }).notNull(),
+})
+
+export const GiangVien = mysqlTable("GiangVien", {
+	id: serial('id').primaryKey(),
+	tenGV: varchar("tenGV", { length: 255 }).notNull(),
+	dienThoai: varchar("dienThoai", { length: 15 }).notNull(),
+	email: varchar("email", { length: 255 }).unique().notNull(),
+	khoaId: int("khoaId").notNull(),
+	bangCapId: int("bangCapId").notNull(),
+}, (table) => ({
+	khoaFK: foreignKey(() => [table.khoaId], () => [Khoa.id]),
+	bangCapFK: foreignKey(() => [table.bangCapId], () => [BangCap.id]),
+}))
